9efb91e91d37575cb53c1cb81d545a3bbba2bbce
LongByte <LongByte@Pen5Quad>
2017-12-06 23:35:21 +0300

    Landing VDSTech

--- landing/index.php
+++ landing/index.php
@@ -22,7 +22,7 @@
                 <div class="col-xs-12 col-sm-6 item" data-block="service-2">
                     <div class="as-table">
                         <div class="cell-middle">
-                            Сервер терминалов <br>
+                            Сервер терминалов<br>
                             <span>(удаленных рабочих столов)</span>
                         </div>
                     </div>
@@ -43,8 +43,9 @@
                 </div>
             </div>
         </div>
-        <div class="container-fluid service">
-            <div class="container" style="display: none" id="service-1">
+        <? // <editor-fold defaultstate="collapsed" desc="1C Cloud"> ?>
+        <div class="container-fluid service" style="display: none" id="service-1">
+            <div class="container">
                 <div class="row">
                     <div class="col-xs-12">
                         <h2>Облачная 1С-Бухгалтерия</h2>
@@ -139,29 +140,257 @@
                         <div class="arrow">></div>
                         <div class="step">готово</div>
                     </div>
-                    
+                </div>
+            </div>
+        </div>
+        <? // </editor-fold> ?>
+        <? // <editor-fold defaultstate="collapsed" desc="Remote"> ?>
+        <div class="container-fluid service" style="display: none" id="service-2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h2>Сервер терминалов (удаленных рабочих столов)</h2>
+                        <p>
+                            Периодически у бизнеса возникает задача организации доступа сотрудников к их рабочему месту не только из офиса. К примеру: сотрудник организации уезжает в командировку и ему необходимо по служебной необходимости осуществить вход на свой рабочий компьютер. Кроме того, перед бизнесом часто возникает задача экономии ресурсов на модернизацию своей IT-инфраструктуры, в частности тех персональных компьютеров, с которыми работают сотрудники компании. Некоторые компании предпочитают использование услуг удаленных сотрудников (Outsourcing), перед ними так же возникает задача организации рабочего места для таких сотрудников.
+                        </p>
+                        <p>
+                            Обращаясь к нам, вы получите.
+                        </p>
+                    </div>
+                    <div class="agvantages clearfix">
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <table>
+                                <tr>
+                                    <td>icon</td>
+                                    <td>
+                                        Удаленный доступ для сотрудников Вашей компании к своему рабочему месту из любой точки земного шара при наличии доступа к сети Интернет 365 дней в году 24 часа 7 дней в неделю.
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <table>
+                                <tr>
+                                    <td>icon</td>
+                                    <td>
+                                        Единое рабочее пространство, удобный обмен рабочей информацией между сотрудниками, в том числе работающим на основе аутсорсинга.
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <table>
+                                <tr>
+                                    <td>icon</td>
+                                    <td>
+                                        Еженедельное резервное копирование виртуального сервера на случай повышенной вирусной активности, и потери данных с возможностью безусловного оперативного возврата на точку резервного копирования при невозможности восстановить работу.
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <table>
+                                <tr>
+                                    <td>icon</td>
+                                    <td>
+                                        Оперативное выделение необходимых ресурсов конкретному сотруднику.
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <table>
+                                <tr>
+                                    <td>icon</td>
+                                    <td>
+                                        Отсутствие необходимости постоянного контроля за IT инфраструктурой клиента.
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <table>
+                                <tr>
+                                    <td>icon</td>
+                                    <td>
+                                        Возможность оперативной организации рабочего места при появлении новых сотрудников.
+                                    </td>
+                                </tr>
+                            </table>
+                        </div> <div class="col-xs-12 col-sm-6 col-md-4">
+                            <table>
+                                <tr>
+                                    <td>icon</td>
+                                    <td>
+                                        Защищенный канал передачи данных между местом, где находится в данный момент сотрудник Вашей компании до нашего центра обработки данных.
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <table>
+                                <tr>
+                                    <td>icon</td>
+                                    <td>
+                                        Возможность получения защищенного доступа к рабочему месту с помощью устройств на базе Android и IOS.
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="col-xs-12">
+                        <p>
+                            Как будет выглядеть услуга на практике:
+                        </p>
+                        <p>
+                            После обращения к нам за получением услуги, мы заключаем с Вами договор на оказание услуг. Затем, в течении трех суток мы организуем для Вас сервер терминалов (удаленных рабочих столов). Вам будут предоставлены реквизиты для доступа для каждого сотрудника Вашей компании. В случае необходимости мы сможем помочь Вам подобрать, поставить, осуществить установку и настройку необходимого Вам для работы программного обеспечения и оборудования.
+                        </p>
+                    </div>
+                    тут будут иконки:
+                    <div class="col-xs-12 flex">
+                        <div class="step">обращение</div>
+                        <div class="arrow">></div>
+                        <div class="step">договор</div>
+                        <div class="arrow">></div>
+                        <div class="step">3 дня</div>
+                        <div class="arrow">></div>
+                        <div class="step">сервер</div>
+                        <div class="arrow">></div>
+                        <div class="step">настройка</div>
+                        <div class="arrow">></div>
+                        <div class="step">готово</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <? // </editor-fold> ?>
+        <? // <editor-fold defaultstate="collapsed" desc="Cloud"> ?>
+        <div class="container-fluid service" style="display: none" id="service-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h2>Облачная рабочая станция</h2>
+                        <p>
+                        </p>
+                        <p>
+                        </p>
+                    </div>
+                    <div class="agvantages clearfix">
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <table>
+                                <tr>
+                                    <td>icon</td>
+                                    <td>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+
+                    </div>
+                    <div class="col-xs-12">
+                        <p>
+                            Как будет выглядеть услуга на практике:
+                        </p>
+                        <p>
+                        </p>
+                    </div>
+                    тут будут иконки:
+                    <div class="col-xs-12 flex">
+                        <div class="step">обращение</div>
+                        <div class="arrow">></div>
+                        <div class="step">договор</div>
+                        <div class="arrow">></div>
+                        <div class="step">3 дня</div>
+                        <div class="arrow">></div>
+                        <div class="step">сервер</div>
+                        <div class="arrow">></div>
+                        <div class="step">настройка</div>
+                        <div class="arrow">></div>
+                        <div class="step">готово</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <? // </editor-fold> ?>
+        <? // <editor-fold defaultstate="collapsed" desc="Web"> ?>
+        <div class="container-fluid service" style="display: none" id="service-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h2>Web-Хостинг</h2>
+                        <p>
+                        </p>
+                        <p>
+                            Мы предлагаем решить эту проблему путем размещения Вашей базы данных в нашем дата-центре.
+                        </p>
+                    </div>
+                    <div class="agvantages clearfix">
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <table>
+                                <tr>
+                                    <td>icon</td>
+                                    <td>
+                                        Ежедневное резервное копирование Вашей базы данных на нашем, защищенном от возможной вирусной активности кластере серверов.
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="col-xs-12">
+                        <p>
+                            Как будет выглядеть услуга на практике:
+                        </p>
+                        <p>
+                            После обращения к нам за получением услуги, мы заключаем с Вами договор на оказание услуг. Затем, в течении трех суток мы организуем для Вас сервер терминалов (удаленных рабочих столов). Вам будут предоставлены реквизиты для доступа для каждого сотрудника Вашей компании. В случае необходимости мы сможем помочь Вам подобрать, поставить, осуществить установку и настройку необходимого Вам для работы программного обеспечения и оборудования.
+                        </p>
+                    </div>
+                    тут будут иконки:
+                    <div class="col-xs-12 flex">
+                        <div class="step">обращение</div>
+                        <div class="arrow">></div>
+                        <div class="step">договор</div>
+                        <div class="arrow">></div>
+                        <div class="step">3 дня</div>
+                        <div class="arrow">></div>
+                        <div class="step">сервер</div>
+                        <div class="arrow">></div>
+                        <div class="step">настройка</div>
+                        <div class="arrow">></div>
+                        <div class="step">готово</div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+        <? // </editor-fold> ?>
+        <div class="container-fluid form">
+            <div class="container">
+                <div class="row">
                     <div class="col-sm-2 hidden-xs"></div>
                     <div class="col-md-1 hidden-xs hidden-sm"></div>
                     <div class="col-xs-12 col-sm-8 col-md-6">
-                        <script id="bx24_form_inline" data-skip-moving="true">
-                            (function (w, d, u, b) {
-                                w['Bitrix24FormObject'] = b;
-                                w[b] = w[b] || function () {
-                                    arguments[0].ref = u;
-                                    (w[b].forms = w[b].forms || []).push(arguments[0])
-                                };
-                                if (w[b]['forms'])
-                                    return;
-                                s = d.createElement('script');
-                                r = 1 * new Date();
-                                s.async = 1;
-                                s.src = u + '?' + r;
-                                h = d.getElementsByTagName('script')[0];
-                                h.parentNode.insertBefore(s, h);
-                            })(window, document, 'https://longbyte.bitrix24.ru/bitrix/js/crm/form_loader.js', 'b24form');
+                        <div class="form">
+                            <script id="bx24_form_inline" data-skip-moving="true">
+                                (function (w, d, u, b) {
+                                    w['Bitrix24FormObject'] = b;
+                                    w[b] = w[b] || function () {
+                                        arguments[0].ref = u;
+                                        (w[b].forms = w[b].forms || []).push(arguments[0])
+                                    };
+                                    if (w[b]['forms'])
+                                        return;
+                                    s = d.createElement('script');
+                                    r = 1 * new Date();
+                                    s.async = 1;
+                                    s.src = u + '?' + r;
+                                    h = d.getElementsByTagName('script')[0];
+                                    h.parentNode.insertBefore(s, h);
+                                })(window, document, 'https://longbyte.bitrix24.ru/bitrix/js/crm/form_loader.js', 'b24form');
 
-                            b24form({"id": "7", "lang": "ru", "sec": "oaynr4", "type": "inline"});
-                        </script>
+                                b24form({"id": "7", "lang": "ru", "sec": "oaynr4", "type": "inline"});
+                            </script>
+                        </div>
                     </div>
                     <div class="col-sm-2 hidden-xs"></div>
                     <div class="col-md-1 hidden-xs hidden-sm"></div>
--- landing/js/script.js
+++ landing/js/script.js
@@ -2,5 +2,7 @@ $(function () {
     $('[data-block]').click(function () {
         $(this).closest('.container-fluid').fadeOut();
         $('#' + $(this).data('block')).fadeIn();
+        $('.form').fadeIn();
+        $('body').removeClass().addClass($(this).data('block'));
     });
 });
