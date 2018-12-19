2ff13cb522c03753400a3d294fb9ffc97692d4a6
LongByte <ilex.chesnokov@gmail.com>
2018-12-19 21:21:43 +0300

    Генератор паролей

---
+++ longbyte.ru/public_html/pwdgen/index.php
@@ -0,0 +1,32 @@
+<?
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("генератор паролей");
+?>
+
+<script>
+    function generatePassword() {
+        var length = $('#length').val(),
+            charset = $('charlist').val(),
+            retVal = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+            retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+        $('#result').val(retVal);
+    }
+
+    $(function () {
+        $('#gogen').click(function () {
+            generatePassword();
+        });
+    });
+</script>
+
+Список символов: <input type="text" id="charlist" value="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-"><br>
+Длина: <input type="number" id="length" value="10" min="1"><br>
+<button id="gogen">Сгенерировать</button><br>
+Длина: <input type="text" id="result" readonly value=""><br>
+
+
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
