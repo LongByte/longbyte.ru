60b3544867b5b270f9c9bb29d9edfdbac89a3534
LongByte <ilex.chesnokov@gmail.com>
2019-03-23 21:42:09 +0300

    Попытки сделать ORM для свойств инфоблока

---
+++ shared/local/php_interface/lib/Iblockv2PropertyTables.php
@@ -0,0 +1,18 @@
+<?
+
+namespace Bitrix\Iblock;
+
+use Bitrix\Main,
+    Bitrix\Main\Localization\Loc;
+
+Loc::loadMessages(__FILE__);
+
+class Iblockv2PropertyTables extends Main\Entity\DataManager {
+
+    public static $iblockId = 0;
+    
+    public static function getMap() {
+        
+    }
+
+}
