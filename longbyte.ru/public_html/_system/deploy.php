fc33c4ffce3411c84b89165230a5d503ab9423fa
LongByte <ilex.chesnokov@gmail.com>
2019-07-14 21:23:48 +0300

    deploy: логирование, переход на пуш

--- longbyte.ru/public_html/_system/deploy.php
+++ longbyte.ru/public_html/_system/deploy.php
@@ -1,5 +1,10 @@
 <?
 
+use Bitrix\Main\IO;
+use Bitrix\Main\Application;
+
+define("NOT_CHECK_PERMISSIONS", true);
+
 $_SERVER["DOCUMENT_ROOT"] = dirname(__DIR__);
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
@@ -9,8 +14,20 @@ $obRequest = $obContext->getRequest();
 $strData = $obRequest->getInput();
 $obData = json_decode($strData);
 
-if ($obData->pullrequest->destination->branch->name == 'master') {
-    shell_exec('~/deploy.sh');
-}
+$obLogFile = new IO\File(Application::getDocumentRoot() . '/_system/deploy.log');
 
+if (isset($obData->push)) {
+    foreach ($obData->push->changes as $obChange) {
+        if ($obChange->new->name == 'master') {
+            $obLogFile->putContents(PHP_EOL . '============' . PHP_EOL, IO\File::APPEND);
+            $obLogFile->putContents(date('d.m.Y H:i:s') . PHP_EOL, IO\File::APPEND);
+            $obLogFile->putContents('Branch: ' . $obChange->new->name . PHP_EOL, IO\File::APPEND);
+            $obLogFile->putContents('Commits:' . PHP_EOL, IO\File::APPEND);
+            foreach ($obChange->commits as $obCommit) {
+                $obLogFile->putContents($obCommit->hash . ' [' . $obCommit->date . ']' . ' ' . $obCommit->author->user->display_name . ': ' . $obCommit->message . PHP_EOL, IO\File::APPEND);
+            }
+            shell_exec('~/deploy.sh');
+        }
+    }
+}
 ?>
