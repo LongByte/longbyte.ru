45c2363d02f46c8b2b77bb8f524c2559e22fa471
LongByte <ilex.chesnokov@gmail.com>
2020-02-24 12:49:40 +0300

    Webp

--- shared/local/php_interface/classes/Webp.php
+++ shared/local/php_interface/classes/Webp.php
@@ -13,21 +13,25 @@ use Bitrix\Main\IO;
 class Webp {
 
     /**
-     * 
+     * Массовая конвертация всех картинок.
      * @param string $content
      */
     public static function convertAllToWebp(&$content) {
 
         if (self::_checkSupport()) {
             $arPatterns = array(
-                '/<img[^>]*src="([^"]+\.(jpg|jpeg|png|bmp))"[^>]*>/',
+                '/<img[^>]* src="([^"]+\.(jpg|jpeg|png|bmp))"[^>]*>/',
+                '/<img[^>]* data-src="([^"]+\.(jpg|jpeg|png|bmp))"[^>]*>/',
                 '/<[^>]*style="[^"]*url\(([^)]+\.(jpg|jpeg|png|bmp))\)[^"]*"[^>]*>/',
             );
 
             foreach ($arPatterns as $strPattern) {
                 preg_match_all($strPattern, $content, $arMatches);
 
-                foreach ($arMatches[1] as $strPath) {
+                foreach ($arMatches[1] as $keyMatch => $strPath) {
+                    if (strpos($arMatches[0][$keyMatch], 'data-webp="skip"') !== false) {
+                        continue;
+                    }
                     $strWebpPath = self::convertToWebp($strPath);
                     $content = str_replace($strPath, $strWebpPath, $content);
                 }
@@ -36,40 +40,65 @@ class Webp {
     }
 
     /**
-     * 
+     * Конвертация форматов.
      * @param string $strSrc
      * @return string
      */
     public static function convertToWebp($strSrc) {
-        $strNewSrc = $strSrc;
-
-        if (self::_checkSupport()) {
-            $strSrc = ToLower($strSrc);
-            if (IO\File::isFileExists(Application::getDocumentRoot() . $strSrc)) {
-                if (strpos($strSrc, '.png')) {
-                    $obImage = imagecreatefrompng(Application::getDocumentRoot() . $strSrc);
-                    $strNewSrc = str_replace('.png', '.webp', $strSrc);
-                } elseif (strpos($strSrc, '.bmp')) {
-                    $obImage = imagecreatefrombmp(Application::getDocumentRoot() . $strSrc);
-                    $strNewSrc = str_replace('.bmp', '.webp', $strSrc);
-                } elseif (strpos($strSrc, '.jpg') !== false || strpos($strSrc, '.jpeg') !== false) {
-                    $obImage = imagecreatefromjpeg(Application::getDocumentRoot() . $strSrc);
-                    $strNewSrc = str_replace(array('.jpg', '.jpeg'), '.webp', $strSrc);
-                }
-                if ($obImage) {
-                    if (!IO\File::isFileExists(Application::getDocumentRoot() . $strNewSrc)) {
-                        imagewebp($obImage, Application::getDocumentRoot() . $strNewSrc, false);
-                    }
-                    imagedestroy($obImage);
-                }
-            }
+        if (!self::_checkSupport() || !IO\File::isFileExists(Application::getDocumentRoot() . $strSrc)) {
+            return $strSrc;
         }
 
-        return $strNewSrc;
+        $strTargetSrc = preg_replace('/\.[^\.]+$/i', '.webp', $strSrc);
+        if (IO\File::isFileExists(Application::getDocumentRoot() . $strTargetSrc)) {
+            return self::_checkCorrectImage($strSrc, $strTargetSrc);
+        }
+
+        if (strpos($strSrc, '.png')) {
+            $obImage = imagecreatefrompng(Application::getDocumentRoot() . $strSrc);
+
+            if (self::_isPhp56BetaEnabled()) {
+                list($iWidth, $iHeight) = getimagesize(Application::getDocumentRoot() . $strSrc);
+                $obJpgImage = imagecreatetruecolor($iWidth, $iHeight);
+                $obWhite = imagecolorallocate($obJpgImage, 255, 255, 255);
+                imagefilledrectangle($obJpgImage, 0, 0, $iWidth, $iHeight, $obWhite);
+                imagecopy($obJpgImage, $obImage, 0, 0, 0, 0, $iWidth, $iHeight);
+
+                $strTempSrc = str_replace('.png', '_tmp.jpg', $strSrc);
+                imagejpeg($obJpgImage, Application::getDocumentRoot() . $strTempSrc, 100);
+                $obImage = imagecreatefromjpeg(Application::getDocumentRoot() . $strTempSrc);
+            }
+        } elseif (strpos($strSrc, '.bmp')) {
+            $obImage = imagecreatefrombmp(Application::getDocumentRoot() . $strSrc);
+        } elseif (strpos($strSrc, '.jpg') !== false || strpos($strSrc, '.jpeg') !== false) {
+            $obImage = imagecreatefromjpeg(Application::getDocumentRoot() . $strSrc);
+        }
+        if ($obImage) {
+            imagewebp($obImage, Application::getDocumentRoot() . $strTargetSrc, 90);
+            imagedestroy($obImage);
+        }
+
+        return self::_checkCorrectImage($strSrc, $strTargetSrc);
     }
 
     /**
-     * 
+     * Проверка, что файл существует и имеет размер больше 0 (сконвертировался корректно)
+     * @param string $strSrc
+     * @param string $strTargetSrc
+     * @return string
+     */
+    private static function _checkCorrectImage($strSrc, $strTargetSrc) {
+        $obTargetFile = new IO\File(Application::getDocumentRoot() . $strTargetSrc);
+        if ($obTargetFile->isExists()) {
+            if ($obTargetFile->getSize() > 0) {
+                return $strTargetSrc;
+            }
+        }
+        return $strSrc;
+    }
+
+    /**
+     * Проверка поддержки.
      * @global \CMain $APPLICATION
      * @return bool
      */
@@ -84,9 +113,19 @@ class Webp {
             function_exists('imagewebp') &&
             strpos($obServer->get('HTTP_ACCEPT'), 'image/webp') !== false &&
             strpos($APPLICATION->GetCurDir(), '/bitrix/') === false &&
-            $APPLICATION->GetProperty('disable_webp') != 'Y' &&
-            intval(phpversion()) >= 7
+            $APPLICATION->GetProperty('enable_webp') != 'N'
             );
+    }
+
+    /**
+     * Возможность включить конвертацию на php 5.6.
+     * На php 5.6 проблема с прозрачностью png. При включении этой опции конвертация идет через jpg с потерей прозрачности фона. Но всегда есть шанс, что что-то пойдет не так.
+     * @global \CMain $APPLICATION
+     * @return bool
+     */
+    private static function _isPhp56BetaEnabled() {
+        global $APPLICATION;
+        return $APPLICATION->GetProperty('enable_webp_php56_beta') == 'Y';
     }
 
 }
