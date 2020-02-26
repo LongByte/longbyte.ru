eb9ac0ba291a90f47667186dda90349f0599489a
LongByte <ilex.chesnokov@gmail.com>
2020-02-27 01:54:50 +0300

    Webp

--- shared/local/php_interface/classes/Webp.php
+++ shared/local/php_interface/classes/Webp.php
@@ -11,98 +11,23 @@ use Bitrix\Main\IO;
  *
  */
 class Webp {
-
-    /**
-     * Массовая конвертация всех картинок.
-     * @param string $content
+    /*
+     * @var $obSourceFile IO\File
      */
-    public static function convertAllToWebp(&$content) {
 
-        if (self::_checkSupport()) {
-            $arPatterns = array(
-                '/<img[^>]* src="([^"]+\.(jpg|jpeg|png|bmp))"[^>]*>/',
-                '/<img[^>]* data-src="([^"]+\.(jpg|jpeg|png|bmp))"[^>]*>/',
-                '/<[^>]*style="[^"]*url\(([^)]+\.(jpg|jpeg|png|bmp))\)[^"]*"[^>]*>/',
-            );
+    private $obSourceFile = null;
 
-            foreach ($arPatterns as $strPattern) {
-                preg_match_all($strPattern, $content, $arMatches);
-
-                foreach ($arMatches[1] as $keyMatch => $strPath) {
-                    if (strpos($arMatches[0][$keyMatch], 'data-webp="skip"') !== false) {
-                        continue;
-                    }
-                    $strWebpPath = self::convertToWebp($strPath);
-                    $content = str_replace($strPath, $strWebpPath, $content);
-                }
-            }
-        }
-    }
-
-    /**
-     * Конвертация форматов.
-     * @param string $strSrc
-     * @return string
+    /*
+     * @var $obTargetFile IO\File
      */
-    public static function convertToWebp($strSrc) {
-        if (!self::_checkSupport() || !IO\File::isFileExists(Application::getDocumentRoot() . $strSrc)) {
-            return $strSrc;
-        }
-
-        $strTargetSrc = preg_replace('/\.[^\.]+$/i', '.webp', $strSrc);
-        if (IO\File::isFileExists(Application::getDocumentRoot() . $strTargetSrc)) {
-            return self::_checkCorrectImage($strSrc, $strTargetSrc);
-        }
-
-        if (strpos($strSrc, '.png')) {
-            $obImage = imagecreatefrompng(Application::getDocumentRoot() . $strSrc);
-
-            if (self::_isPhp56BetaEnabled()) {
-                list($iWidth, $iHeight) = getimagesize(Application::getDocumentRoot() . $strSrc);
-                $obJpgImage = imagecreatetruecolor($iWidth, $iHeight);
-                $obWhite = imagecolorallocate($obJpgImage, 255, 255, 255);
-                imagefilledrectangle($obJpgImage, 0, 0, $iWidth, $iHeight, $obWhite);
-                imagecopy($obJpgImage, $obImage, 0, 0, 0, 0, $iWidth, $iHeight);
-
-                $strTempSrc = str_replace('.png', '_tmp.jpg', $strSrc);
-                imagejpeg($obJpgImage, Application::getDocumentRoot() . $strTempSrc, 100);
-                $obImage = imagecreatefromjpeg(Application::getDocumentRoot() . $strTempSrc);
-            }
-        } elseif (strpos($strSrc, '.bmp')) {
-            $obImage = imagecreatefrombmp(Application::getDocumentRoot() . $strSrc);
-        } elseif (strpos($strSrc, '.jpg') !== false || strpos($strSrc, '.jpeg') !== false) {
-            $obImage = imagecreatefromjpeg(Application::getDocumentRoot() . $strSrc);
-        }
-        if ($obImage) {
-            imagewebp($obImage, Application::getDocumentRoot() . $strTargetSrc, 90);
-            imagedestroy($obImage);
-        }
-
-        return self::_checkCorrectImage($strSrc, $strTargetSrc);
-    }
-
-    /**
-     * Проверка, что файл существует и имеет размер больше 0 (сконвертировался корректно)
-     * @param string $strSrc
-     * @param string $strTargetSrc
-     * @return string
-     */
-    private static function _checkCorrectImage($strSrc, $strTargetSrc) {
-        $obTargetFile = new IO\File(Application::getDocumentRoot() . $strTargetSrc);
-        if ($obTargetFile->isExists()) {
-            if ($obTargetFile->getSize() > 0) {
-                return $strTargetSrc;
-            }
-        }
-        return $strSrc;
-    }
+    private $obTargetFile = null;
 
     /**
      * Проверка поддержки.
      * @global \CMain $APPLICATION
      * @return bool
      */
-    private static function _checkSupport() {
+    public static function checkSupport() {
 
         global $APPLICATION;
         $obServer = Context::getCurrent()->getServer();
@@ -118,14 +43,124 @@ class Webp {
     }
 
     /**
-     * Возможность включить конвертацию на php 5.6.
-     * На php 5.6 проблема с прозрачностью png. При включении этой опции конвертация идет через jpg с потерей прозрачности фона. Но всегда есть шанс, что что-то пойдет не так.
-     * @global \CMain $APPLICATION
-     * @return bool
+     * Массовая конвертация всех картинок на странице.
+     * @param string $content
      */
-    private static function _isPhp56BetaEnabled() {
-        global $APPLICATION;
-        return $APPLICATION->GetProperty('enable_webp_php56_beta') == 'Y';
+    public static function convertAllToWebp(&$content) {
+
+        if (self::checkSupport()) {
+            $arPatterns = array(
+                '/<img[^>]* src="([^"]+\.(jpg|jpeg|png|bmp))"[^>]*>/',
+                '/<img[^>]* data-src="([^"]+\.(jpg|jpeg|png|bmp))"[^>]*>/',
+                '/<[^>]*style="[^"]*url\(([^)]+\.(jpg|jpeg|png|bmp))\)[^"]*"[^>]*>/',
+            );
+
+            foreach ($arPatterns as $strPattern) {
+                preg_match_all($strPattern, $content, $arMatches);
+
+                foreach ($arMatches[1] as $keyMatch => $strPath) {
+                    if (strpos($arMatches[0][$keyMatch], 'data-webp="skip"') !== false) {
+                        continue;
+                    }
+                    $obWebp = new self($strPath);
+                    $strWebpPath = $obWebp->getWebpPath();
+                    $content = str_replace($strPath, $strWebpPath, $content);
+                }
+            }
+        }
+    }
+
+    /**
+     * 
+     * @param string $strSrc
+     */
+    public function __construct(string $strSrc) {
+        $this->obSourceFile = new IO\File(Application::getDocumentRoot() . $strSrc);
+        $this->obTargetFile = new IO\File(Application::getDocumentRoot() . preg_replace('/\.[^\.]+$/i', '.webp', $strSrc));
+    }
+
+    /**
+     * Конвертация форматов.
+     * @return string
+     */
+    public function getWebpPath() {
+        $obImage = null;
+        if (!self::checkSupport() || !$this->getSourceFile()->isExists()) {
+            return $this->getSourceSrc();
+        }
+
+        if ($this->getTargetFile()->isExists()) {
+            return $this->checkCorrectImage();
+        }
+
+        if ($this->isPng()) {
+            $obImage = imagecreatefrompng($this->getSourceFile()->getPath());
+        } elseif ($this->isBmp()) {
+            $obImage = imagecreatefrombmp($this->getSourceFile()->getPath());
+        } elseif ($this->isJpg()) {
+            $obImage = imagecreatefromjpeg($this->getSourceFile()->getPath());
+        }
+        if (!is_null($obImage)) {
+            imagewebp($obImage, $this->getTargetFile()->getPath(), 90);
+            imagedestroy($obImage);
+        }
+
+        return $this->checkCorrectImage();
+    }
+
+    /**
+     * 
+     * @return IO\File
+     */
+    private function getSourceFile() {
+        return $this->obSourceFile;
+    }
+
+    /**
+     * 
+     * @return IO\File
+     */
+    private function getTargetFile() {
+        return $this->obTargetFile;
+    }
+
+    /**
+     * 
+     * @return string
+     */
+    private function getSourceSrc() {
+        return str_replace(Application::getDocumentRoot(), '', $this->getSourceFile()->getPath());
+    }
+
+    /**
+     * 
+     * @return string
+     */
+    private function getTargetSrc() {
+        return str_replace(Application::getDocumentRoot(), '', $this->getTargetFile()->getPath());
+    }
+
+    /**
+     * Проверка, что файл существует и имеет размер больше 0 (сконвертировался корректно)
+     * @return string
+     */
+    private function checkCorrectImage() {
+        if ($this->getTargetFile()->isExists() && $this->getTargetFile()->getSize() > 0) {
+            return $this->getTargetSrc();
+        }
+        return $this->getSourceSrc();
+    }
+
+    private function isPng() {
+        return $this->getSourceFile()->getExtension() == 'png' && $this->getSourceFile()->getContentType() == 'image/png';
+    }
+
+    private function isBmp() {
+        return $this->getSourceFile()->getExtension() == 'bmp' && $this->getSourceFile()->getContentType() == 'image/bmp';
+    }
+
+    private function isJpg() {
+        return in_array($this->getSourceFile()->getExtension(), array('jpg', 'jpeg')) && $this->getSourceFile()->getContentType() == 'image/jpeg';
     }
 
 }
