d8913a0080ee6f5c414dafc3a1f88ab4ea537e35
LongByte <ilex.chesnokov@gmail.com>
2020-05-31 02:16:55 +0300

    Sensors fix

--- longbyte.ru/public_html/api/sensors/sensors_server_check.sh
+++ longbyte.ru/public_html/api/sensors/sensors_server_check.sh
@@ -3,6 +3,7 @@ if (( $PID > 0 )); then
     echo 'pid found...';
     if ! [ -d /proc/$PID/ ]; then
         echo 'restarting...'
+        cd ~/web/longbyte.ru/public_html/api/sensors
         ./sensors_server_start.sh
     fi
 fi
